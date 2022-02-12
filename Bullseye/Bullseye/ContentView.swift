72b885a5cf38f29371e41c594fee0405e5a8665f
Christian Özelt <oezelt@mediamarktsaturn.com>
2022-02-12 14:37:43 +0100

    :sparkles: Challenge: SwiftUI State

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -8,18 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPopoverVisible: Bool = false
+    @State private var isWhoIsThereVisible: Bool = false
+
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4)
+                .font(.footnote)
             Text("89")
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .kerning(-1)
             HStack {
                 Text("1")
+                    .font(.body)
+                    .bold()
                 Slider(value: .constant(89), in: 1.0...100.0)
                 Text("100")
+                    .font(.body)
+                    .bold()
             }
-            Button(action: {}) {
-                Text("Hit me")
-            }
+            Button("Hit me"){
+                self.isPopoverVisible = true
+            }.alert(isPresented: $isPopoverVisible, content: {
+                Alert(title: Text("Hello There"),
+                      message: Text("This is my first popup"),
+                      dismissButton: .default(Text("Awesome!")))
+            })
+            Button("knock know"){
+                self.isWhoIsThereVisible = true
+            }.alert(isPresented: $isWhoIsThereVisible, content: {
+                Alert(title: Text("Who is there?"),
+                      message: Text("your mum"),
+                      dismissButton: .default(Text("Awesome!")))
+            })
+            
         }
     }
 }
